77f9eec1ce4bad4bb5d2178855966f641bdae4f4
arshbhushan <arshbhushan@gmail.com>
2025-01-04 16:14:52 +0530

    Initial commit

---
+++ src/main/java/com/decentraV/decentraVault/DecentraVaultApplication.java
@@ -0,0 +1,13 @@
+package com.decentraV.decentraVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecentraVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecentraVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/decentraV/decentraVault/DecentraVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decentraV.decentraVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecentraVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
