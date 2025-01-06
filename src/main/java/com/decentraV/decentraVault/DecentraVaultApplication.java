84bcc26ae476d5a1d55d9f28d11407fe5d045b0b
arshbhushan <arshbhushan@gmail.com>
2025-01-06 11:43:40 +0530

    Enabled REST Templates.

--- src/main/java/com/decentraV/decentraVault/DecentraVaultApplication.java
+++ src/main/java/com/decentraV/decentraVault/DecentraVaultApplication.java
@@ -2,12 +2,21 @@ package com.decentraV.decentraVault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 public class DecentraVaultApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DecentraVaultApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
