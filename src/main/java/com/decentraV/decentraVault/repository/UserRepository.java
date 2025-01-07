0a06a37f45b516577339bac5c618e3310a0f90a8
arshbhushan <arshbhushan@gmail.com>
2025-01-08 02:33:45 +0530

    Created UserService  and repo.

---
+++ src/main/java/com/decentraV/decentraVault/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.decentraV.decentraVault.repository;
+
+import com.decentraV.decentraVault.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    User findByUserName(String username);
+
+    void deleteByUserName(String username);
+}
---
+++ src/main/java/com/decentraV/decentraVault/service/UserService.java
@@ -0,0 +1,68 @@
+package com.decentraV.decentraVault.service;
+
+import com.decentraV.decentraVault.entity.User;
+import com.decentraV.decentraVault.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Slf4j
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private static final PasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
+    //private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    public void saveNewEntry(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("USER"));
+        userRepository.save(user);
+    }
+
+    public boolean saveNewUser(User user) {
+        try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setRoles(Arrays.asList("USER"));
+            userRepository.save(user);
+            return true;
+        }catch (Exception e){
+            log.info("Error saving new user");
+            return false;
+
+        }
+    }
+
+    public void saveAdmin(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("USER", "ADMIN"));
+        userRepository.save(user);
+    }
+
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(ObjectId id) {
+        return userRepository.findById(id);
+    }
+
+    public void deleteById(ObjectId id) {
+        userRepository.deleteById(id);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUserName(username);
+    }
+}
