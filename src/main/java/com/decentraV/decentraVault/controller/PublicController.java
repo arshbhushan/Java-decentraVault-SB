10527a0597a7f0acc6622018569e96b28bf3ad48
arshbhushan <arshbhushan@gmail.com>
2025-01-09 00:20:23 +0530

    Created a public controller for user login and signup

--- src/main/java/com/decentraV/decentraVault/controller/PublicController.java
+++ src/main/java/com/decentraV/decentraVault/controller/PublicController.java
@@ -1,18 +1,63 @@
 package com.decentraV.decentraVault.controller;
 
+import com.decentraV.decentraVault.entity.User;
+import com.decentraV.decentraVault.service.UserService;
+import com.decentraV.decentraVault.utils.JwtUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/public")
 @Slf4j
 public class PublicController {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @GetMapping("/health-check")
     public String healthCheck() {
-        log.info("Health-check endpoint accessed.");
         return "OK";
+    }
+
+    @PostMapping("/signup")
+    public void signup(@RequestBody User user) {
+        userService.saveNewEntry(user);
+    }
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword()));
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+            log.info("username : " + user.getUserName());
+            log.info("Password : " + user.getPassword());
+            String jwt= jwtUtil.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        }catch(Exception e){
+            log.error("Exception occurred while CreteAuthenticationToken",e);
+            return new ResponseEntity<>("Incorrect username or password",HttpStatus.BAD_REQUEST);
+        }
     }
 }
