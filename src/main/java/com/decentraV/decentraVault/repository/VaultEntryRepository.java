9dc48224928d63fa867e0b60691d418cf862084e
arshbhushan <arshbhushan@gmail.com>
2025-01-11 23:17:07 +0530

    Created a vault entry rep

--- src/main/java/com/decentraV/decentraVault/repository/VaultEntryRepository.java
+++ src/main/java/com/decentraV/decentraVault/repository/VaultEntryRepository.java
@@ -1,8 +1,11 @@
 package com.decentraV.decentraVault.repository;
 
+import com.decentraV.decentraVault.entity.User;
 import com.decentraV.decentraVault.entity.VaultEntry;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
 
 public interface VaultEntryRepository extends MongoRepository<VaultEntry, ObjectId> {
 }
